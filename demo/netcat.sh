240e794e701349e426aa991554c2befed81a9cd0
Eric Mignot <eric.mignot@gmail.com>
2020-03-06 09:57:57 -0800

    Travis?

--- demo/netcat.sh
+++ demo/netcat.sh
@@ -2,13 +2,13 @@
 
 function test_netcat_can_handle_http {
 	local message=(
-            "HTTP/1.1 200 OK"
-            "Content-Type: text/html; charset=UTF-8"
-            "Content-Length: 31"
-            "Server: netcat"
-            ""
-            "<html><body>hello</body></html>"
-            ""
+		"HTTP/1.1 200 OK"
+		"Content-Type: text/html; charset=UTF-8"
+		"Content-Length: 31"
+		"Server: netcat"
+		""
+		"<html><body>hello</body></html>"
+		""
 	)    
     printf "%s\n" "${message[@]}" | nc -l -p 8333 &
     local body=`curl --silent --noproxy localhost, http://localhost:8333`
