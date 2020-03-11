c882cf38bf39c307487feffb744314e803a2019b
Eric Mignot <eric.mignot@gmail.com>
2020-03-11 09:43:42 -0700

    renamed

--- share/testing/run-tests.sh
+++ share/testing/run-tests.sh
@@ -3,9 +3,9 @@
 function test_run {
     : > ./share/testing/actual
 
-    ./share/testing/lib/run.sh ./share/testing/data/several-tests >> ./share/testing/actual
-    ./share/testing/lib/run.sh ./share/testing/data/failure >> ./share/testing/actual
-    ./share/testing/lib/run.sh ./share/testing/data/test-only >> ./share/testing/actual
+    ./share/testing/lib/test.sh ./share/testing/data/several-tests >> ./share/testing/actual
+    ./share/testing/lib/test.sh ./share/testing/data/failure >> ./share/testing/actual
+    ./share/testing/lib/test.sh ./share/testing/data/test-only >> ./share/testing/actual
 
     delta=`diff ./share/testing/actual ./share/testing/expected`
 
