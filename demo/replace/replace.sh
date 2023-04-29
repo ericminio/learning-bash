6ac373df348583ca7c768447ad02028be7b0e6c9
ericminio <eric.mignot@gmail.com>
2023-04-29 07:10:36 -0700

    working with bash and ksh

--- demo/replace/replace.sh
+++ demo/replace/replace.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 function replace_token {
-    find $3 -type f -exec sed -i "s/$1/$2/g" {} \;
+    echo "$1 -> $2"
+    for file in $(grep -rlH "$1" $3); do
+        echo $file
+        local after=$(sed "s/$1/$2/g" $file)
+        echo $after > $file
+    done
 }
 
 function duplicate_and_adjust {
@@ -17,6 +22,7 @@ function seed_incoming {
     mkdir $1
     echo "/before/me/after me" > $1/import
     echo "VARIABLE_WITH_THIS_NAME=42 # this is great" > $1/variable
+    echo "do not touch me" > $1/no
 }
 
 function test_replace_can_focus_on_path {
@@ -33,4 +39,12 @@ function test_replace_can_focus_on_variable {
     local actual=$(cat ./demo/replace/actual/variable)
 
     assertequals "$actual" "VARIABLE_WITH_THAT_NAME=42 # this is great"
+}
+
+function test_replace_is_strict_with_regex_matching {
+    seed_incoming ./demo/replace/incoming
+    duplicate_and_adjust ./demo/replace/incoming ./demo/replace/actual
+    local actual=$(cat ./demo/replace/actual/no)
+
+    assertequals "$actual" "do not touch me"
 }
