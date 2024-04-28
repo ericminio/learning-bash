6a07bb012ca1a8b31e5b426f0c653cf2e5fd0c33
ericminio <eric.mignot@gmail.com>
2024-04-27 20:42:23 -0700

    json now uses trim

--- about/trim/trim.tests.sh
+++ about/trim/trim.tests.sh
@@ -23,16 +23,16 @@ function test_trim_preserves_inner_spaces {
     assertequals "$actual" "1 2 3"
 }
 
-function test_trim_can_remove_trailing_cr_lf {
-    local input=$'123\r\n\n'    
-    local actual=$(printf "%s" "$input" | trim)    
+function test_trim_needs_help_to_remove_trailing_cr_lf {
+    local input=$'123  \r\n\n  '    
+    local actual=$(printf "%s" "$input" | oneliner | trim)    
     
     assertequals "$actual" "123"
 }
 
-function test_trim_can_remove_leading_cr_lf {
-    local input=$'\r\n\n123'
-    local actual=$(printf "%s" "$input" | trim)
+function test_trim_needs_help_to_remove_leading_cr_lf {
+    local input=$'  \r\n\n  123'
+    local actual=$(printf "%s" "$input" | oneliner | trim)
     
     assertequals "$actual" "123"
 }
@@ -45,11 +45,11 @@ function test_trim_digests_grep_one_line {
     assertequals "$actual" "hello"
 }
 
-function test_trim_digests_grep_two_lines {
-    echo "hello world     " > ./about/incoming
-    echo "hello my friend" >> ./about/incoming
-    echo "bye for now    " >> ./about/incoming
-    local actual=$(cat ./about/incoming | grep "hello" | trim)    
+function test_trim_digests_grep_two_lines_with_help {
+    echo "  hello world     " > ./about/incoming
+    echo "hello my friend  " >> ./about/incoming
+    echo "bye for now      " >> ./about/incoming
+    local actual=$(cat ./about/incoming | grep "hello" | oneliner | shrink | trim)    
 
     assertequals "$actual" "hello world hello my friend"
 }
--- dist/utils.sh
+++ dist/utils.sh
@@ -8,22 +8,22 @@ function shrink {
     sed -E 's/\s+/ /g'
 }
 function trim {
-    oneliner | shrink | sed -E 's/(^\s*|\s*$)//g'
+    sed -E 's/(^\s*|\s*$)//g'
 }
 
 function opening {
-    sed -E -e 's/\{\s*/{ /g'
+    sed -E 's/\{\s*/{ /g'
 }
 function closing {
-    sed -E -e 's/\s*\}/ }/g'
+    sed -E 's/\s*\}/ }/g'
 }
 function comma {
-    sed -E -e 's/,\s*/, /g'
+    sed -E 's/,\s*/, /g'
 }
 function colon {
-    sed -E -e 's/:\s+/:/g'
+    sed -E 's/:\s+/:/g'
 }
 function json {
-    oneliner | opening | closing  | comma | colon | sed -E 's/(^[ ]*|[ ]*$)//g'
+    oneliner | opening | closing  | comma | colon | trim
 }
 
