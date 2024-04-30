66bbf9dd126804fb30aa2746473acfdfedcf984b
Eric Mignot <eric.mignot@gmail.com>
2024-04-30 11:55:41 -0700

    waiting

---
+++ about/waiting/when-done.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source './dist/waiting.sh'
+
+function wait_for_this {
+    ls -l | grep "about"
+}
+
+function test_only_waiting_stops_when_done {    
+    result=$(waiting "about folder" wait_for_this)
+
+    assertequals "$result" "about folder is ready"
+}
+
---
+++ dist/waiting.sh
@@ -0,0 +1,12 @@
+function waiting {
+    ready=0
+    while [ "$ready" -eq "0" ]
+    do    
+        ready=`$2 | wc -l`
+        if [ "$ready" -eq "0" ]; then    
+            echo "Waiting for $1"
+            sleep 1;
+        fi
+    done;
+    echo "$1 is ready";
+}
