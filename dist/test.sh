3e8e76eda9fb3ae6174a7b43d0239b56c1419ec7
ericminio <eric.mignot@gmail.com>
2024-08-11 06:18:47 -0700

    highlight failing test

--- dist/test.sh
+++ dist/test.sh
@@ -3,12 +3,13 @@
 pass_color="\033[0;102;30m"
 fail_color="\033[0;101;30m"
 reset_color="\033[0m"
+red="\033[0;91m"
 
 function assertequals {
     if [ "$1" = "$2" ]; then
         return 0
     else
-        FAILED_EXPECTATION="\n${fail_color}FAIL${reset_color}\n\nExpected: $2 \nBut was : $1"
+        FAILED_EXPECTATION="${fail_color}FAIL${reset_color}\n\nExpected: $2 \nBut was : $1"
         return 1
     fi
 }
@@ -31,6 +32,7 @@ function run_test {
     echo $1
     $1
     if (( $? != 0 )); then
+        echo -e "\n${red}$1${reset_color}"
         echo -e "$FAILED_EXPECTATION"
         exit 1
     fi
