12374d1685aa6fb7c2e32f8821e4090dddcfc4e2
Eric Mignot <eric.mignot@gmail.com>
2024-08-13 15:16:16 -0700

    about if and regexp

--- about/if.sh
+++ about/if.sh
@@ -8,9 +8,45 @@ function yes {
         echo "no"
     fi
 }
+function yes_string {
+    value="hello world"
+    check="hello world"
+    if [ "$value" = "$check" ]; then
+        echo "yes"
+    else
+        echo "no"
+    fi
+}
+function yes_matching_regexp {
+    value="hello world!"
+    pattern="world"
+    if [[ "$value" =~ $pattern ]]; then
+        echo "yes"
+    else
+        echo "no"
+    fi
+}
 function no {
     value=15
     if [ $value -eq 42 ]; then
+        echo "yes"
+    else
+        echo "no"
+    fi
+}
+function no_string {
+    value="hello world"
+    check="hello"
+    if [ "$value" = "$check" ]; then
+        echo "yes"
+    else
+        echo "no"
+    fi
+}
+function no_matching_regexp {
+    value="hello world!"
+    pattern="world$"
+    if [[ "$value" =~ $pattern ]]; then
         echo "yes"
     else
         echo "no"
@@ -25,6 +61,30 @@ function test_if_number_equals_42 {
 
 function test_if_number_does_not_equal_42 {
     local actual=$(no)
+
+    assertequals "$actual" "no"
+}
+
+function test_if_strings_equal {
+    local actual=$(yes_string)
+
+    assertequals "$actual" "yes"
+}
+
+function test_if_strings_do_not_equal {
+    local actual=$(no_string)
+
+    assertequals "$actual" "no"
+}
+
+function test_if_string_matches {
+    local actual=$(yes_matching_regexp)
+
+    assertequals "$actual" "yes"
+}
+
+function test_if_string_does_not_match {
+    local actual=$(no_matching_regexp)
 
     assertequals "$actual" "no"
 }
