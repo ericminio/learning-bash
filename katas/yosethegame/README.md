3a6504de9fa4688902d27bbbe20edcf1505cb89e
Eric Mignot <eric.mignot@gmail.com>
2025-06-11 14:56:34 -0700

    20

--- katas/yosethegame/README.md
+++ katas/yosethegame/README.md
@@ -1,7 +1,9 @@
-[![yosethegame](https://img.shields.io/badge/yosethegame-10-00A000?style=flat)](https://yosethegame.com)
+[![yosethegame](https://img.shields.io/badge/yosethegame-20-00A000?style=flat)](https://yosethegame.com)
 
 ```
 docker compose -f support/ci/docker-compose.yml run --rm -it --name bash -p 8333:8333 bash
 cd katas/yosethegame/
 ./server.sh
 ```
+
+`npm run play http://127.0.0.1:8333`
--- katas/yosethegame/server.sh
+++ katas/yosethegame/server.sh
@@ -1,11 +1,37 @@
 #!/bin/bash
 
+# inspired by https://dev.to/leandronsp/building-a-web-server-in-bash-part-ii-parsing-http-14kg
+
+rm -f response
+mkfifo response
 
 function home {
   cat index.html
 }
 
-while true; do { \
-  echo -ne "HTTP/1.0 200 OK\r\nContent-Type: text/html\r\n\r\n"; \
-  home; } | nc -l -p 8333 -q 1 ; \ 
-done
+function pong {
+  echo '{"pong":"hi there!"}'
+}
+
+function handler {
+  while read line; do
+    trline=`echo $line | tr -d '[\r\n]'`    
+
+    HEADLINE_REGEX='(.*?)\s(.*?)\sHTTP.*?'
+    if [[ $trline =~ $HEADLINE_REGEX ]]; then
+      REQUEST=$(echo $trline | sed -E "s/$HEADLINE_REGEX/\1 \2/")
+      echo "$REQUEST"
+      break
+    fi
+  done
+
+  case "$REQUEST" in
+    "GET /") RESPONSE="HTTP/1.1 200\r\nContent-Type: text/html\r\n\r\n$(home)" ;;
+    "GET /ping") RESPONSE="HTTP/1.1 200\r\nContent-Type: application/json\r\n\r\n$(pong)" ;;
+          *) RESPONSE="HTTP/1.1 404 NotFound\r\n\r\n\r\nNot Found" ;;
+  esac
+
+  echo -ne $RESPONSE > response
+}
+
+while true; do cat response | nc -l -p 8333 | handler ; done
