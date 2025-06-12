2938762cfebc15524503cb967581771de5ca89b3
Eric Mignot <eric.mignot@gmail.com>
2025-06-11 19:56:57 -0700

    score 40

--- katas/yosethegame/README.md
+++ katas/yosethegame/README.md
@@ -1,4 +1,4 @@
-[![yosethegame](https://img.shields.io/badge/yosethegame-20-00A000?style=flat)](https://yosethegame.com)
+[![yosethegame](https://img.shields.io/badge/yosethegame-40-00A000?style=flat)](https://yosethegame.com)
 
 ```
 docker compose -f support/ci/docker-compose.yml run --rm -it --name bash -p 8333:8333 bash
--- katas/yosethegame/server.sh
+++ katas/yosethegame/server.sh
@@ -13,21 +13,67 @@ function pong {
   echo '{"pong":"hi there!"}'
 }
 
+function decompose {
+  local number="$1"
+
+  local factors=()
+  local factor=2
+
+  while [ $((number % factor)) -eq 0 ]; do
+    factors+=($factor)
+    number=$((number / factor))
+  done
+
+  local joined_with_comma=$(IFS=,; echo "${factors[*]}")
+  
+  echo "$joined_with_comma"
+}
+
+function primeFactors {
+  local url="$1"
+  local NUMBER_REGEX='.*number=(.*?)$'
+  local number=$(echo $url | sed -E "s/$NUMBER_REGEX/\1/")
+  local NUMBER_REGEX='^[0-9]+$'
+  if [[ ! $number =~ $NUMBER_REGEX ]]; then
+    echo "HTTP/1.1 400\r\nContent-Type: application/json\r\n\r\n{\"number\":\"$number\",\"error\":\"not a number\"}"
+    return
+  fi
+
+  echo "HTTP/1.1 200\r\nContent-Type: application/json\r\n\r\n{\"number\":$number,\"decomposition\":[$(decompose $number)]}"
+}
+
+function route {
+  local request="$1"
+
+  if [[ $request == "GET /" ]]; then
+    echo "home"
+  elif [[ $request == "GET /ping" ]]; then
+    echo "ping"
+  elif [[ $request =~ 'primeFactors' ]]; then
+    echo "primeFactors"
+  else
+    echo "not-found"
+  fi
+}
+
 function handler {
   while read line; do
     trline=`echo $line | tr -d '[\r\n]'`    
 
     HEADLINE_REGEX='(.*?)\s(.*?)\sHTTP.*?'
     if [[ $trline =~ $HEADLINE_REGEX ]]; then
-      REQUEST=$(echo $trline | sed -E "s/$HEADLINE_REGEX/\1 \2/")
-      echo "$REQUEST"
+      METHOD=$(echo $trline | sed -E "s/$HEADLINE_REGEX/\1/")
+      URL=$(echo $trline | sed -E "s/$HEADLINE_REGEX/\2/")
+      echo $METHOD $URL
+      REQUEST="$METHOD $URL"
       break
     fi
   done
 
-  case "$REQUEST" in
-    "GET /") RESPONSE="HTTP/1.1 200\r\nContent-Type: text/html\r\n\r\n$(home)" ;;
-    "GET /ping") RESPONSE="HTTP/1.1 200\r\nContent-Type: application/json\r\n\r\n$(pong)" ;;
+  case $(route "$REQUEST") in
+    "home") RESPONSE="HTTP/1.1 200\r\nContent-Type: text/html\r\n\r\n$(home)" ;;
+    "ping") RESPONSE="HTTP/1.1 200\r\nContent-Type: application/json\r\n\r\n$(pong)" ;;
+    "primeFactors") RESPONSE=$(primeFactors $URL) ;;
           *) RESPONSE="HTTP/1.1 404 NotFound\r\n\r\n\r\nNot Found" ;;
   esac
 
